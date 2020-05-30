96afdc9a81a40bfaec81535c706adaf1b6bf33a7
Jan G <apollg3@gmail.com>
2020-05-30 16:52:36 +0800

    Setup db with sequelize.

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+
+import typeDefs from './schema';
+import resolvers from './resolvers';
+import models from './models';
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+server.applyMiddleware({ app });
+
+models.sequelize.sync().then(function () {
+  app.listen({ port: 8080 }, () =>
+    console.log(`🚀 Server ready at http://localhost:8080${server.graphqlPath}`)
+  );
+});
